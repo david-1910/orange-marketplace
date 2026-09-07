094dd7d523fff9cae76dad59bca9e20cff39215f
david-1910 <dovud.1910@gmail.com>
2026-09-07 14:40:53 +0500

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { cn } from '@/shared/lib';
+
+export function App() {
+  return (
+    <div className={cn('flex min-h-svh items-center justify-center')}>
+      <h1 className="text-3xl font-medium text-orange-500">
+        Приложение готово к FSD
+      </h1>
+    </div>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 5000,
+  },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
